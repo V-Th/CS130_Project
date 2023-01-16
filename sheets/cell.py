861ca899dfb6a50d718c5fbff3d54b260a0907d0
Vinny Thai <vthai@caltech.edu>
2023-01-16 13:34:47 -0800

    Added workbook and cellerrors

--- sheets/cell.py
+++ sheets/cell.py
@@ -1,8 +1,105 @@
-import decimal
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from cellgraph import *
+from cellerrortype import *
+from cellerror import *
+import decimal 
 import lark
-from cellerror import CellError
-from cellerrortype import CellErrorType
+import logging
 from lark.visitors import visit_children_decor
+
+class Cell():
+    # constructor 
+    def __init__(self, workbook, sheet_name: str, location: str, graph):
+        self.contents = None
+        self.display_contents = None
+        self.value_evaluator = None
+        self.value = None
+        self.graph = graph
+        self.workbook = workbook
+        self.sheet_name = sheet_name
+        self.location = location
+        self.dependencies = []
+
+    def __repr__(self):
+        return self.contents
+
+    # set contents as given string
+    def set_contents(self, contents: str):
+        self.contents = contents.upper()
+        self.display_contents = contents
+        # instiate the right CellValue class type depending on the case
+        if self.contents[0] == '=':
+            try:
+                self.value_evaluator = CellValueFormula()
+                parser = lark.Lark.open('formulas.lark', start='formula')
+                evaluator = DependencyFinder(self)
+                tree = parser.parse(self.contents)
+                evaluator.cell(tree)
+            except AssertionError:
+                detail = f'Bad reference to non-existent cell'
+                self.value = CellError(CellErrorType.BAD_REFERENCE, detail)
+        elif self.contents[0] == '\'':
+            self.value_evaluator = CellValueString()
+        else:
+            try:
+                decimal.Decimal(self.contents)
+                self.value_evaluator = CellValueNumber()
+            except decimal.InvalidOperation:
+                self.value_evaluator = CellValueString()
+        self.value = self.value_evaluator.get_value(self)
+        # check for dependencies
+
+    # return literal contents of cell
+    def get_contents(self):
+        return self.display_contents
+
+    # return literal contents of cell
+    def get_display_contents(self):
+        return self.display_contents
+
+    # inform workbook of dependency between this cell and another cell
+    def add_dependency(self, location: str, sheet_name: str = None):
+        if location == None:
+            logging.info("Cell: add_dependency: could not add dependency - NoneType")
+            return
+        if sheet_name == None:
+            sheet_name = self.sheet_name
+        dependent_cell = self.workbook.add_dependency(self, location, sheet_name)
+        self.dependencies.append(dependent_cell)
+    
+    # return value calculated from cell contents
+    def get_value(self):
+        if self.value_evaluator is None:
+            return decimal.Decimal()
+        if self.value == None:
+            self.value = self.value_evaluator.get_value(self)
+        return self.value
+    
+class CellValueString():
+    def get_value(self, cell: Cell):
+        try:
+            if cell.display_contents[0] == '\'':
+                return cell.display_contents[1:]
+            err_type = CellErrorType(cell.display_contents.upper())
+            detail = f"Written error: {cell.display_contents}"
+            return CellError(err_type, detail)
+        except:
+            return cell.display_contents
+
+class CellValueNumber():
+    def get_value(self, cell: Cell):
+        parser = lark.Lark.open('formulas.lark', start='base')
+        tree = parser.parse(cell.display_contents)
+        return decimal.Decimal(tree.children[0])
+
+class CellValueFormula():
+    def get_value(self, cell: Cell):
+        parser = lark.Lark.open('formulas.lark', start='formula')
+        evaluator = FormulaEvaluator(cell.workbook, cell.sheet_name)
+        tree = parser.parse(cell.contents)
+        value = evaluator.visit(tree)
+        return value
 
 def check_arithmetic_input(value):
     if isinstance(value, (decimal.Decimal, CellError)):
@@ -25,7 +122,10 @@ def check_inputs(value1, value2):
     else:
         return check_v1, check_v2
 
-class FormulaEvaluater(lark.visitors.Interpreter):
+class FormulaEvaluator(lark.visitors.Interpreter):
+    def __init__(self, workbook, sheet_name):
+       self.workbook = workbook
+       self.sheet_name = sheet_name
 
     @visit_children_decor
     def add_expr(self, values):
@@ -56,7 +156,11 @@ class FormulaEvaluater(lark.visitors.Interpreter):
                 return CellError(CellErrorType.DIVIDE_BY_ZERO, detail, err)
         else:
             assert False, 'Unexpected operator: ' + values[1]
-    
+
+    @visit_children_decor
+    def concat_expr(self, values):
+        return values[0] + values[1]
+
     @visit_children_decor
     def unary_op(self, values):
         v1 = check_arithmetic_input(values[1])
@@ -68,60 +172,47 @@ class FormulaEvaluater(lark.visitors.Interpreter):
             return -v1
         else:
             assert False, 'Unexpected operator: ' + values[0]
-    
-    @visit_children_decor
-    def concat_expr(self, values):
-        return values[0] + values[1]
 
     def error(self, tree):
         return CellError(CellErrorType(tree.children[0]), tree.children[0])
 
     def number(self, tree):
         return decimal.Decimal(tree.children[0])
-
+    
     def string(self, tree):
-        return tree.children[0][1: -1]
+        return tree.children[0].value[1:-1]
     
     def parens(self, tree):
-        return self.visit(tree.children[0])
+        result = self.visit(tree.children[0])
+        return result
 
-    def cell(self, tree):
+    def sheetname(self, tree):
         return tree.children[0]
 
-def cell_eval(content):
-    parser = lark.Lark.open('formulas.lark', start='formula')
-    f_evaluater = FormulaEvaluater()
-    
-    if content[0] == '=':
-        try:
-            return f_evaluater.visit(parser.parse(content))
-        except Exception as err:
-            detail = f'The formula could not be parsed: {content}'
-            return CellError(CellErrorType.PARSE_ERROR, detail, err)
-    elif content[0] == '\'':
-        return content[1:]
-    else:
-        try:
-            return decimal.Decimal(content)
-        except decimal.InvalidOperation:
-            return content
+    def cell(self, tree):
+        if (len(tree.children) == 1):
+            other_cell = self.workbook.get_dependent_cell_value(self.sheet_name, tree.children[0])
+            if other_cell == None:
+                return decimal.Decimal()
+            else:
+                return other_cell
+        else:
+            other_cell = self.workbook.get_dependent_cell_value(tree.children[0], tree.children[1])
+            if other_cell == None:
+                return decimal.Decimal()
+            else:
+                return other_cell 
 
-class Cell:
-    
-    def __init__(self, content: str):
-        self._content = content
-        self._value = cell_eval(self._content)
-        self._dependencies = []
-    
-    def get_value(self):
-        return self._value
+class DependencyFinder(lark.visitors.Interpreter):
+    def __init__(self, parent_cell):
+        self.parent_cell = parent_cell
 
-    def set_content(self, new_content):
-        self._content = new_content
-        self._value = cell_eval(self._content)
-
-    def update_value(self):
-        self._value = cell_eval(self._content)
-
-    def get_dependencies(self):
-        return self._dependencies
+    def cell(self, tree):
+        values = self.visit_children(tree)
+        if type(values) == None:
+            logging.info("DependencyFinder: cell: NoneType value detected")
+            return
+        #print('parent: ' + self.parent_cell.location + ' values: ' + str(values))
+        #print(values[0])
+        if (values != None and tree.data == 'cell'):
+            self.parent_cell.add_dependency(values[0])
---
+++ sheets/workbook.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python3
+from cell import *
+from cellerror import *
+from cellgraph import *
+import lark
+import logging
+
+class Workbook():
+    def __init__(self):
+        self.workbook = self
+        self.sheets = {}
+        self.extents = {}
+        self.graph = CellGraph()
+        self.display_sheets = {}
+
+    def num_sheets(self) -> int:
+        return len(self.sheets)
+
+    def list_sheets(self) -> list[str]:
+        return self.sheets.keys()
+    
+    def __list_cells(self, sheet_name) -> list[str]:
+        return self.sheets[sheet_name].keys()
+    
+    def sheet_name_exists(self, sheet_name: str) -> bool:
+        return sheet_name.upper() in (s.upper() for s in self.sheets.keys())
+    
+    def location_exists(self, sheet_name:str, location: str) -> bool:
+        return location.upper() in (l.upper() for l in self.sheets[sheet_name.upper()].keys())
+    
+    # returns false if given sheet name has leading or trailing whitespace, already exists in the workbook, 
+    # or contains an illegal character 
+    # true otherwise
+    def is_valid_sheet(self, sheet_name) -> bool:
+        if type(sheet_name) != 'str':
+            return False
+        if self.sheet_name_exists(sheet_name):
+            logging.info("Workbook: is_valid_sheet: sheet name already exists")
+            return False
+        if (sheet_name.strip() != sheet_name):
+            logging.info("Workbook: is_valid_sheet: leading or trailing whitespace")
+            return False
+        if ('/' in sheet_name) or ('<' in sheet_name) or ('=' in sheet_name) or ('>' in sheet_name) or ('[' in sheet_name) or (']' in sheet_name) or ('+' in sheet_name):
+            logging.info("Workbook: is_valid_sheet: illegal charcter in sheet name")
+            return False
+        return True
+    
+    # returns whether the given string represents a valid location in the sheet
+    def is_valid_location(self, location: str) -> bool:
+        if len(location) > 8 or len(location) < 2:
+            logging.info("Workbook: is_valid_location: invalid cell location, too long or too short")
+            return False
+        parser = lark.Lark.open('formulas.lark', start='expression')
+        try:
+            tree = parser.parse(location)
+            return True
+        except:
+            print("Workbook: is_valid_location: could not recognize cell reference")
+            return False 
+    
+    # creates new empty spreadsheet if the given sheet_name is valid 
+    # returns a the index and name of the new spreadsheet
+    def new_sheet(self, sheet_name:str = None) -> tuple[int, str]:
+        if self.is_valid_sheet(sheet_name):
+            self.sheets[sheet_name.upper()] = {}
+            return (self.num_sheets(), sheet_name)
+        else:
+            n = 1
+            while(True):
+                if "SHEET" + str(n) not in self.sheets and n <= self.num_sheets() + 1:
+                    self.sheets["SHEET" + str(n)] = {}
+                    self.display_sheets["SHEET" + str(n)] = "Sheet" + str(n)
+                    return (n, "Sheet" + str(n))
+                n += 1
+
+    # delete the given spreadsheet            
+    def del_sheet(self, sheet_name: str) -> None:
+        if self.sheet_name_exists(sheet_name):
+            self.sheets.pop(sheet_name.upper())
+
+    # return number of rows and columns in the given spreadsheet
+    def get_sheet_extent(self, sheet_name: str) -> tuple[int, int]:
+        if self.sheet_name_exists(sheet_name):
+            return self.extents[sheet_name.upper()]
+        logging.info("Workbook: get_sheet_extent: sheet name not in extents")
+        return None
+    
+    # when a spreadsheet is added or deleted, update the extent of that sheet 
+    def __update_sheet_extent(self, sheet_name: str) -> None:
+        if self.sheet_name_exists(sheet_name):
+            a = 0
+            b = 0
+            new_extent = (0,0)
+            n = 0
+            for i in self.sheets[sheet_name.upper()].keys():
+                for j in range(len(i)):
+                    if i[j].upper().isalpha():
+                        a += ord(i[j].upper()) - 64 + (26 * n)
+                    else:
+                        b = int(i[j:]) 
+                        break
+                if (a,b) > new_extent:
+                    new_extent = (a,b)
+            self.extents[sheet_name.upper()] = new_extent
+    
+    # set the cell of the given location to the given contents
+    # return true if successful, false otherwise
+    def set_cell_contents(self, sheet_name: str, location: str, contents:str) -> None:
+        if not self.sheet_name_exists(sheet_name):
+            return False
+        if not self.is_valid_location(location):
+            return False
+        # check if the sheet already has a cell, if not create one
+        if not self.location_exists(sheet_name, location):
+            self.sheets[sheet_name.upper()][location.upper()] = Cell(self.workbook, sheet_name, location, self.graph)
+        # update the cell contents
+        self.sheets[sheet_name.upper()][location.upper()].set_contents(contents)
+        self.__update_sheet_extent(sheet_name.upper())
+        return
+
+    # return the cell object at a particular location
+    def add_dependency(self, src_cell: Cell, location: str, sheet_name: str) -> Cell:
+        assert self.sheet_name_exists(sheet_name), sheet_name
+        assert self.is_valid_location(location.upper())
+        # check if the sheet already has a cell, if not create one
+        if not self.location_exists(sheet_name, location):
+            self.sheets[sheet_name.upper()][location.upper()] = Cell(self.workbook, sheet_name, location, self.graph)
+        dest_cell = self.sheets[sheet_name.upper()][location.upper()]
+        self.graph.add_edge(src_cell, dest_cell)
+        return dest_cell
+    
+    # return the contents of the cell at the given location
+    def get_cell_contents(self, sheet_name: str, location: str):
+        if self.sheet_name_exists(sheet_name) and self.location_exists(sheet_name, location):
+            return self.sheets[sheet_name.upper()][location.upper()].get_contents()
+        return None
+
+    # return the value of the cell at the given location    
+    def get_cell_value(self, sheet_name: str, location: str):
+        # parameter validation
+        if not self.sheet_name_exists(sheet_name):
+            logging.info("Workbook: get_cell_value: sheet_name does not exist")
+            return None
+        if not self.location_exists(sheet_name, location):
+            logging.info("Workbook: get_cell_value: location does not exist")
+            return None
+        # get strongly connected component set
+        self.graph.SCC()
+        # get values in order of the strongly connected set
+        for set in self.graph.setList:
+            if len(set) > 1:
+                return CellError("#CIRCREF!", "Circular reference detected")
+            for j in set:
+                self.sheets[j.sheet_name.upper()][j.location.upper()].get_value()
+        return self.sheets[sheet_name.upper()][location.upper()].get_value()
+    
+    def get_dependent_cell_value(self, sheet_name:str, location:str):
+        return self.sheets[sheet_name.upper()][location.upper()].value
